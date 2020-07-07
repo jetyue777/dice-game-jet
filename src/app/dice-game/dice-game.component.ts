6218dab926a1b4aae43163a30d3471380924624e
jetyue <jetyue777@gmail.com>
2020-07-06 21:40:11 -0400

    first commit

---
+++ src/app/dice-game/dice-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice-game',
+  templateUrl: './dice-game.component.html',
+  styleUrls: ['./dice-game.component.scss']
+})
+export class DiceGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
